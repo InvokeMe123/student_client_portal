8769a8fc028dc1e740209aeab27b6ac1e0ed17da
Sagar Khadka <khadkasagar212@gmail.com>
2024-09-17 08:54:38 +0545

    student landing page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginForm from './template/login';
 import UserRegistration from './template/registration';
 import TeacherLandingPage from './components/TeacherLandingPage';
-
+import StudentLandingPage from './components/StudentLandingPage';  // Import the student landing page
 
 const App = () => {
   return (
@@ -11,7 +11,8 @@ const App = () => {
       <Switch>
         <Route path="/login" exact component={LoginForm} />
         <Route path="/register" component={UserRegistration} />
-        <Route path="/teacher" component={TeacherLandingPage} /> {/* Add Teacher Landing Page Route */}
+        <Route path="/teacher" component={TeacherLandingPage} />
+        <Route path="/student" component={StudentLandingPage} /> {/* Add Student Landing Page Route */}
         <Route path="/" exact component={LoginForm} />
       </Switch>
     </Router>
